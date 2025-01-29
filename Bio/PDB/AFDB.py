394c7026db4c127f9dfbda8095eae7bb76c425f1
valentin994 <valentin.vareskic@gmail.com>
2025-01-29 12:08:11 +0100

    added functionality of listing accessions in the latest directory, added missing raise_for_status

--- Bio/PDB/AFDB.py
+++ Bio/PDB/AFDB.py
@@ -3,8 +3,11 @@
 import argparse
 import asyncio
 from pprint import pprint
+from ftplib import FTP
 
 import httpx
+
+# TODO implement with aioftp so everything is async
 
 
 class AFDB:
@@ -14,7 +17,7 @@ class AFDB:
 
     def __init__(
         self,
-        ftp_server: str = "https://ftp.ebi.ac.uk/pub/databases/alphafold",
+        ftp_server: str = "ftp.ebi.ac.uk",
         version: str = "latest",
     ) -> None:
         """TODO"""
@@ -25,8 +28,11 @@ class AFDB:
     async def get_readme(self) -> str:
         """TODO"""
         try:
-            request_readme = await self.api_client.get(f"{self.ftp_server}/README.txt")
+            request_readme = await self.api_client.get(
+                f"https://{self.ftp_server}/pub/databases/alphafold/README.txt"
+            )
             request_readme.raise_for_status()
+            await self.api_client.aclose()
         except httpx.HTTPStatusError as err:
             return f"Exception occured for fetching the README. {err}"
         except httpx.RequestError as err:
@@ -37,17 +43,24 @@ class AFDB:
         """TODO"""
         try:
             request_metadata = await self.api_client.get(
-                f"{self.ftp_server}/download_metadata.json"
+                f"https://{self.ftp_server}/pub/databases/alphafold/download_metadata.json"
             )
+            request_metadata.raise_for_status()
+            await self.api_client.aclose()
         except httpx.HTTPStatusError as err:
             return f"Exception occured for fetching the README. {err}"
         except httpx.RequestError as err:
             return f"Error occurred during the request: {err}"
         return request_metadata.json()
 
-    async def bulk_download(self):
+    def list_accessions(self) -> list[str]:
         """TODO"""
-        raise NotImplementedError
+        directories = []
+        with FTP("ftp.ebi.ac.uk") as ftp:
+            ftp.login()
+            ftp.cwd(f"/pub/databases/alphafold/{self.version}/")
+            directories = ftp.nlst()
+        return directories
 
 
 async def main():
@@ -65,12 +78,23 @@ async def main():
         action="store_true",
         help="Get the readme from alphahold ftp server",
     )
+    parser.add_argument(
+        "-ls",
+        "--list_acs",
+        action="store_true",
+        help="List accessions from the ftp server",
+    )
+    # TODO add a way to setup version
     afdb = AFDB()
     args = parser.parse_args()
     if args.metadata:
         pprint(await afdb.get_metadata_json())
     elif args.readme:
         print(await afdb.get_readme())
+    elif args.list_acs:
+        for accession in afdb.list_accessions():
+            if accession.startswith("UP"):
+                print(accession)
 
 
 if __name__ == "__main__":
